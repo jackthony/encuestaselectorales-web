a387934bda8704f7d38624d4e96548f6f617d0c7
Jack Aguilar <43307950+jackthony@users.noreply.github.com>
2026-07-21 02:36:33 -0500

    fix(prod): ship db bootstrap for live rounds

---
+++ config/db.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'dsn' => 'mysql:host=srv469.hstgr.io;dbname=u185878096_encuestas;charset=utf8mb4',
+    'user' => 'u185878096_encuestas_app',
+    'pass' => 'Codexito1234.',
+];
